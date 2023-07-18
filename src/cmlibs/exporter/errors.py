9d99b4222bf710978756dfbae6cfa35c4db973f5
Hugh Sorby <h.sorby@auckland.ac.nz>
2023-07-18 17:43:55 +1200

    Add VTK file exporter.

--- src/cmlibs/exporter/errors.py
+++ src/cmlibs/exporter/errors.py
@@ -7,5 +7,17 @@ class ExportWebGLError(ExportError):
     pass
 
 
+class ExportVTKError(ExportError):
+    pass
+
+
+class ExportSTLError(ExportError):
+    pass
+
+
+class ExportWavefrontError(ExportError):
+    pass
+
+
 class ExportImageError(ExportError):
     pass
--- tests/test_exporter.py
+++ tests/test_exporter.py
@@ -2,6 +2,9 @@ import os.path
 import unittest
 
 from cmlibs.exporter import thumbnail
+from cmlibs.exporter import vtk
+from cmlibs.exporter import stl
+from cmlibs.exporter import wavefront
 
 
 here = os.path.abspath(os.path.dirname(__file__))
@@ -24,3 +27,51 @@ class Exporter(unittest.TestCase):
         self.assertTrue(os.path.isfile(thumbnail_file))
 
         os.remove(thumbnail_file)
+
+    def test_vtk(self):
+        argon_document = _resource_path("argon-document.json")
+        output_target = _resource_path("")
+
+        exporter = vtk.ArgonSceneExporter(output_target=output_target)
+        exporter.load(argon_document)
+        exporter.export_vtk()
+
+        cube_file = _resource_path("ArgonSceneExporterVTK_cube.vtk")
+        self.assertTrue(os.path.isfile(cube_file))
+        sphere_file = _resource_path("ArgonSceneExporterVTK_sphere.vtk")
+        self.assertTrue(os.path.isfile(sphere_file))
+
+        os.remove(cube_file)
+        os.remove(sphere_file)
+
+    def test_stl(self):
+        argon_document = _resource_path("argon-document.json")
+        output_target = _resource_path("")
+
+        exporter = stl.ArgonSceneExporter(output_target=output_target)
+        exporter.load(argon_document)
+        exporter.export_stl()
+
+        stl_file = _resource_path("ArgonSceneExporterSTL_zinc_graphics.stl")
+        self.assertTrue(os.path.isfile(stl_file))
+
+        os.remove(stl_file)
+
+    def test_wavefront(self):
+        argon_document = _resource_path("argon-document.json")
+        output_target = _resource_path("")
+
+        exporter = wavefront.ArgonSceneExporter(output_target=output_target)
+        exporter.load(argon_document)
+        exporter.export_wavefront()
+
+        wavefront_file = _resource_path("ArgonSceneExporterWavefront_base.obj")
+        self.assertTrue(os.path.isfile(wavefront_file))
+        cube_file = _resource_path("cube.1.obj")
+        self.assertTrue(os.path.isfile(cube_file))
+        sphere_file = _resource_path("sphere.1.obj")
+        self.assertTrue(os.path.isfile(sphere_file))
+
+        os.remove(wavefront_file)
+        os.remove(cube_file)
+        os.remove(sphere_file)
